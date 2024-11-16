ab7d6a7911855d71d827a35b604394ca743eb5b9
suraj <surajlokhande7744@gmail.com>
2024-11-16 07:47:02 +0530

    basic setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 8080;
+
+// use middlewares 
+app.use(express.json());
+app.use(cors());
+
+// home route 
+app.get('/', (req, res)=>{
+    console.log("welcome to home route")
+    res.send("home Page");
+})
+
+app.listen(port, ()=>{
+    console.log(`server listening on port ${port}`);
+});
